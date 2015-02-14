769cf2a43512593da55a66ae14e6c10ef649194b
Nicolas Besnard <besnard.nicolas@gmail.com>
2015-02-14 12:17:28 +0000

    Initial Commit

---
+++ ChiracBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChiracBoard
+//
+//  Created by Nicolas Besnard on 14/02/2015.
+//  Copyright (c) 2015 Nicolas Besnard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
